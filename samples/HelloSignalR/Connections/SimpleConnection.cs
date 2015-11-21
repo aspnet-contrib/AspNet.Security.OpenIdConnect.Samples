06be66cd96d568ebc0db3f99370af61246282b6a
Dovydas Navickas <dovydas.nav@gmail.com>
2015-11-21 03:56:19 +0200

    Add a new SignalR client/server sample

---
+++ samples/HelloSignalR/Connections/SimpleConnection.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace HelloSignalR.Connections {
+    public class SimpleConnection : PersistentConnection {
+        protected override async Task OnConnected(HttpRequest request, string connectionId) {
+            var identity = request.HttpContext.User.Identity;
+            var status = identity.IsAuthenticated ? "Authenticated" : "Unauthenticated";
+
+            Logger.LogInformation($"{status} connection {connectionId} has just connected.");
+
+            await Connection.Send(connectionId, $"Connection is {status}");
+
+            if (identity.IsAuthenticated) {
+                await Connection.Send(connectionId, $"Authenticated username: {identity.Name}");
+            }
+        }
+
+        protected override async Task OnReceived(HttpRequest request, string connectionId, string data) {
+            var identity = request.HttpContext.User.Identity;
+            var status = identity.IsAuthenticated ? "authenticated" : "unauthenticated";
+            var name = identity.IsAuthenticated ? identity.Name : "client";
+
+            await Connection.Send(connectionId, $"Received an {status} message from {name}: {data}");
+        }
+    }
+}
---
+++ samples/HelloSignalR/Providers/AuthenticationProvider.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using AspNet.Security.OpenIdConnect.Extensions;
+using AspNet.Security.OpenIdConnect.Server;
+using Microsoft.AspNet.Authentication.OpenIdConnect;
+
+namespace HelloSignalR.Providers {
+    public class AuthenticationProvider : OpenIdConnectServerProvider {
+        public override Task ValidateClientAuthentication(ValidateClientAuthenticationContext context) {
+            if (context.ClientId == "AspNetContribSample") {
+                // Note: the context is marked as skipped instead of validated because the client
+                // is not trusted (JavaScript applications cannot keep their credentials secret).
+                context.Skipped();
+            }
+
+            else {
+                // If the client_id doesn't correspond to the
+                // intended identifier, reject the request.
+                context.Rejected();
+            }
+
+            return Task.FromResult(0);
+        }
+
+        public override Task GrantResourceOwnerCredentials(GrantResourceOwnerCredentialsContext context) {
+            var user = new { Id = "users-123", UserName = "AspNet", Password = "contrib" };
+
+            if (context.UserName != user.UserName || context.Password != user.Password) {
+                context.Rejected("Invalid username or password.");
+
+                return Task.FromResult(0);
+            }
+
+            var identity = new ClaimsIdentity(OpenIdConnectDefaults.AuthenticationScheme);
+            identity.AddClaim(ClaimTypes.NameIdentifier, user.Id, "id_token token");
+            identity.AddClaim(ClaimTypes.Name, user.UserName, "id_token token");
+
+            context.Validated(new ClaimsPrincipal(identity));
+
+            return Task.FromResult(0);
+        }
+    }
+}
