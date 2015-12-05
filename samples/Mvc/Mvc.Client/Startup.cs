d4601f4e24c7201372295f0cb13bf71eb5666fb2
Kévin Chalet <kevinchalet@gmail.com>
2015-12-05 02:57:20 +0100

    Copy the Mvc.Client/Mvc.Server samples from the main repository

---
+++ samples/Mvc/Mvc.Client/Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using Microsoft.AspNet.Authentication;
+using Microsoft.AspNet.Authentication.Cookies;
+using Microsoft.AspNet.Authentication.OpenIdConnect;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace Mvc.Client {
+    public class Startup {
+        public void ConfigureServices(IServiceCollection services) {
+            services.Configure<SharedAuthenticationOptions>(options => {
+                options.SignInScheme = "ClientCookie";
+            });
+
+            services.AddAuthentication();
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app) {
+            var factory = app.ApplicationServices.GetRequiredService<ILoggerFactory>();
+            factory.AddConsole();
+
+            app.UseIISPlatformHandler(options => {
+                options.AuthenticationDescriptions.Clear();
+            });
+
+            app.UseStaticFiles();
+
+            // Insert a new cookies middleware in the pipeline to store the user
+            // identity after he has been redirected from the identity provider.
+            app.UseCookieAuthentication(options => {
+                options.AutomaticAuthenticate = true;
+                options.AutomaticChallenge = true;
+                options.AuthenticationScheme = "ClientCookie";
+                options.CookieName = CookieAuthenticationDefaults.CookiePrefix + "ClientCookie";
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                options.LoginPath = new PathString("/signin");
+            });
+
+            app.UseOpenIdConnectAuthentication(options => {
+                options.AuthenticationScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                options.RequireHttpsMetadata = false;
+
+                // Note: these settings must match the application details
+                // inserted in the database at the server level.
+                options.ClientId = "myClient";
+                options.ClientSecret = "secret_secret_secret";
+                options.PostLogoutRedirectUri = "http://localhost:53507/";
+
+                // Use the authorization code flow.
+                options.ResponseType = OpenIdConnectResponseTypes.Code;
+
+                // Note: setting the Authority allows the OIDC client middleware to automatically
+                // retrieve the identity provider's configuration and spare you from setting
+                // the different endpoints URIs or the token validation parameters explicitly.
+                options.Authority = "http://localhost:54540/";
+
+                // Note: the resource property represents the different endpoints the
+                // access token should be issued for (values must be space-delimited).
+                options.Resource = "http://localhost:54540/";
+            });
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ samples/Mvc/Mvc.Server/Startup.cs
@@ -0,0 +1,134 @@
+using System;
+using Microsoft.AspNet.Authentication;
+using Microsoft.AspNet.Authentication.Cookies;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Mvc.Server.Extensions;
+using Mvc.Server.Models;
+using Mvc.Server.Providers;
+
+#if !DNXCORE50
+using NWebsec.Owin;
+#endif
+
+namespace Mvc.Server {
+    public class Startup {
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddEntityFramework()
+                .AddInMemoryDatabase()
+                .AddDbContext<ApplicationContext>(options => {
+                    options.UseInMemoryDatabase();
+                });
+
+            services.Configure<SharedAuthenticationOptions>(options => {
+                options.SignInScheme = "ServerCookie";
+            });
+
+            services.AddAuthentication();
+            services.AddCaching();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app) {
+            var factory = app.ApplicationServices.GetRequiredService<ILoggerFactory>();
+            factory.AddConsole();
+
+            app.UseIISPlatformHandler(options => {
+                options.AuthenticationDescriptions.Clear();
+            });
+
+            app.UseStaticFiles();
+
+            // Create a new branch where the registered middleware will be executed only for API calls.
+            app.UseWhen(context => context.Request.Path.StartsWithSegments(new PathString("/api")), branch => {
+                branch.UseJwtBearerAuthentication(options => {
+                    options.AutomaticAuthenticate = true;
+                    options.AutomaticChallenge = true;
+                    options.RequireHttpsMetadata = false;
+
+                    options.Audience = "http://localhost:54540/";
+                    options.Authority = "http://localhost:54540/";
+                });
+            });
+
+            // Create a new branch where the registered middleware will be executed only for non API calls.
+            app.UseWhen(context => !context.Request.Path.StartsWithSegments(new PathString("/api")), branch => {
+                // Insert a new cookies middleware in the pipeline to store
+                // the user identity returned by the external identity provider.
+                branch.UseCookieAuthentication(options => {
+                    options.AutomaticAuthenticate = true;
+                    options.AutomaticChallenge = true;
+                    options.AuthenticationScheme = "ServerCookie";
+                    options.CookieName = CookieAuthenticationDefaults.CookiePrefix + "ServerCookie";
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                    options.LoginPath = new PathString("/signin");
+                });
+
+                branch.UseGoogleAuthentication(options => {
+                    options.ClientId = "560027070069-37ldt4kfuohhu3m495hk2j4pjp92d382.apps.googleusercontent.com";
+                    options.ClientSecret = "n2Q-GEw9RQjzcRbU3qhfTj8f";
+                });
+
+                branch.UseTwitterAuthentication(options => {
+                    options.ConsumerKey = "6XaCTaLbMqfj6ww3zvZ5g";
+                    options.ConsumerSecret = "Il2eFzGIrYhz6BWjYhVXBPQSfZuS4xoHpSSyD9PI";
+                });
+            });
+
+#if !DNXCORE50
+            app.UseOwinAppBuilder(owin => {
+                // Insert a new middleware responsible of setting the Content-Security-Policy header.
+                // See https://nwebsec.codeplex.com/wikipage?title=Configuring%20Content%20Security%20Policy&referringTitle=NWebsec
+                owin.UseCsp(options => options.DefaultSources(configuration => configuration.Self())
+                                              .ImageSources(configuration => configuration.Self().CustomSources("data:"))
+                                              .ScriptSources(configuration => configuration.UnsafeInline())
+                                              .StyleSources(configuration => configuration.Self().UnsafeInline()));
+
+                // Insert a new middleware responsible of setting the X-Content-Type-Options header.
+                // See https://nwebsec.codeplex.com/wikipage?title=Configuring%20security%20headers&referringTitle=NWebsec
+                owin.UseXContentTypeOptions();
+
+                // Insert a new middleware responsible of setting the X-Frame-Options header.
+                // See https://nwebsec.codeplex.com/wikipage?title=Configuring%20security%20headers&referringTitle=NWebsec
+                owin.UseXfo(options => options.Deny());
+
+                // Insert a new middleware responsible of setting the X-Xss-Protection header.
+                // See https://nwebsec.codeplex.com/wikipage?title=Configuring%20security%20headers&referringTitle=NWebsec
+                owin.UseXXssProtection(options => options.EnabledWithBlockMode());
+            });
+#endif
+
+            app.UseOpenIdConnectServer(options => {
+                options.Provider = new AuthorizationProvider();
+
+                // Note: see AuthorizationController.cs for more
+                // information concerning ApplicationCanDisplayErrors.
+                options.ApplicationCanDisplayErrors = true;
+                options.AllowInsecureHttp = true;
+
+                // Note: by default, tokens are signed using dynamically-generated
+                // RSA keys but you can also use your own certificate:
+                // options.SigningCredentials.AddCertificate(certificate);
+            });
+
+            app.UseMvc();
+
+            app.UseWelcomePage();
+
+            using (var database = app.ApplicationServices.GetService<ApplicationContext>()) {
+                database.Applications.Add(new Application {
+                    ApplicationID = "myClient",
+                    DisplayName = "My client application",
+                    RedirectUri = "http://localhost:53507/signin-oidc",
+                    LogoutRedirectUri = "http://localhost:53507/",
+                    Secret = "secret_secret_secret"
+                });
+
+                database.SaveChanges();
+            }
+        }
+    }
+}
